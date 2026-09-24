c39cb992ca711fd21275b58e1360f4b4c92deb72
YoussefAlaa-magisk <yasserahmed55525@gmail.com>
2026-09-24 19:24:08 +0300

    Add customize.sh for installation messages

---
+++ customize.sh
@@ -0,0 +1,15 @@
+echo "hello from nothing module !"
+echo "installing..."
+sleep 1
+echo "1"
+sleep 1
+echo "2"
+sleep 1
+echo "3"
+sleep 1
+echo "wait"
+sleep 1
+echo "1"
+sleep 1
+echo "2"
+echo "done you need to reboot to get nothing ♡"
