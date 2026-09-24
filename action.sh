91f68e1694929134513443bf8d661b9db1575f7d
YoussefAlaa-magisk <yasserahmed55525@gmail.com>
2026-09-24 20:45:43 +0300

    Add files via upload

---
+++ action.sh
@@ -0,0 +1,20 @@
+MODDIR="$(find /data/adb/modules -type d -name samsung_SuperUI_boot)"
+
+chmod 755 "$MODDIR/get-v"
+"$MODDIR/get-v"
+
+echo "Nothing"
+sleep 5
+echo "are you here ?"
+sleep 10
+echo "SO ?"
+sleep 3
+echo "exiting in : "
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1 
+echo "1"
+sleep 1
+exit
